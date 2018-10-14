bf83333b02df54b75a89039dc4db554f2d327fdb
maher460 <maher460@gmail.com>
2018-10-14 11:55:25 -0400

    bla bla

--- js/index.js
+++ js/index.js
@@ -13,18 +13,67 @@ $( document ).ready(function() {
     var WAIT_FOR_ME_F = false;
     var waiting_for_me = false;
     var waiting_for_ajax = false;
+    var data = [['EU', 'Paris', 'France', '$$$', 'Temperate', 'Museums', 'No Coast'], 
+		        ['EU', 'London', 'Britain', '$$ ', 'Cold', 'Theatre', 'No Coast'], 
+		        ['EU', 'Venice', 'Italy', '$$$', 'Temperate', 'History', 'Coast'], 
+		        ['EU', 'Bucharest', 'Romania', '$ ', 'Temperate', 'History', 'No Coast'], 
+		        ['EU', 'Podgorica', 'Montenegro', '$ ', 'Temperate', 'Outdoors', 'No Coast'], 
+		        ['NA', 'New York', 'USA', '$$$', 'Cold', 'Theatre', 'No Coast'], 
+		        ['NA', 'Boulder ', 'USA', '$$', 'Cold', 'Outdoors', 'No Coast'], 
+		        ['NA', 'Cancun', 'Mexico', '$', 'Hot', 'Party', 'Coast'], 
+		        ['NA', 'San Francisco', 'USA', '$$', 'Temperate', 'Party', 'Coast'], 
+		        ['NA', 'Los Angeles', 'USA', '$$$', 'Temperate', 'Party', 'Coast'], 
+		        ['NA', 'Kansas City', 'USA', '$$', 'Temperate', 'Outdoors', 'No Coast'], 
+		        ['EA', 'Hong Kong', 'China', '$$$', 'Hot', 'Shopping', 'No Coast'], 
+		        ['EA', 'Tokyo', 'Japan', '$ ', 'Temperate', 'Shopping', 'No Coast'], 
+		        ['EA', 'Shenzhen', 'China', '$$ ', 'Hot', 'Technology', 'No Coast'], 
+		        ['EA', 'Mumbai', 'India', '$$ ', 'Hot', 'Culture', 'Coast'], 
+		        ['EA', 'New Delhi', 'India', '$$$ ', 'Hot', 'Culture', 'No Coast'], 
+		        ['SA', 'Rio de Janeiro', 'Brasil', '$$$', 'Temperate', 'Soccer', 'No Coast'], 
+		        ['SA', 'Panama City', 'Panama', '$ ', 'Hot', 'Canal', 'Coast'], 
+		        ['SA', 'Lima', 'Peru', '$$ ', 'Hot', 'Outdoors', 'No Coast'], 
+		        ['SA', 'Guatemala City', 'Guatemala', '$$ ', 'Hot', 'Culture', 'No Coast'], 
+		        ['SA', 'Buenos Aires', 'Argentina', '$$$ ', 'Temperate', 'Soccer', 'Coast'], 
+		        ['AF', 'Lagos', 'Nigeria', '$$$', 'Hot', 'Outdoors', 'No Coast'], 
+		        ['AF', 'Johannesburg', 'South Africa', '$$ ', 'Temperate', 'Culture', 'No Coast'], 
+		        ['AF', 'Cairo ', 'Egypt', '$', 'Temperate', 'Pyramids', 'Coast'], 
+		        ['AF', 'Nairobi', 'Kenya', '$$$', 'Hot', 'Outdoors', 'No Coast'], 
+		        ['AF', 'Algiers', 'Algeria', '$', 'Hot', 'Outdoors', 'Coast']]
+
+	var activities = [['Culture', "img/activities/culture.jpg"],
+					  ['History', "img/activities/history.jpg"],
+					  ['Museums', "img/activities/museums.jpg"],
+					  ['Outdoors', "img/activities/outdoors.jpg"],
+					  ['Party', "img/activities/party.jpg"],
+					  ['Shopping', "img/activities/shopping.jpg"],
+					  ['Soccer', "img/activities/soccer.jpg"],
+					  ['Technology', "img/activities/technology.jpg"],
+					  ['theatre', "img/activities/theatre.jpg"]]
+
+	var cities = [['Algiers', "img/cities/algiers - Edited.jpg"],
+				  ['Boulder', "img/cities/boulder - Edited.jpg"],
+				  ['Bucharest', "img/cities/bucharest - Edited.jpg"],
+				  ['Buenos Aires', "img/cities/buenos aires - Edited.jpg"],
+				  ['Cairo', "img/cities/cairo - Edited.jpg"],
+				  ['Cancun', "img/cities/cancun - Edited.jpg"],
+				  ['Guatemalacity', "img/cities/guatemalacity - Edited.jpg"],
+				  ['Hong Kong', "img/cities/hongkong - Edited.jpg"]]
+
+
     var people_info = [["Watson", "img/watson2_pro_pic.png"],
     				   ["Maher", "img/maher_pro_pic.jpg"],
     				   ["Dan", "img/dan_pro_pic.jpg"],
     				   ["Logan", "img/logan_pro_pic.jpg"],
     				   ["Matei", "img/matei_pro_pic.jpg"]]
+
     var telescript = [[WATSON, "Hi, how are you today?", WAIT_FOR_ME_T, null],
     				  [WATSON, "So, tell me some of your travel interests.", WAIT_FOR_ME_T, show_interests],
     				  [WATSON, "How about we invite some of your other friends to plan a trip together?", WAIT_FOR_ME_T, null],
     				  [WATSON, "Cool, adding them up now!", WAIT_FOR_ME_T, invite_friends],
     				  [FRIEND1, "Heyy, what's poppinng, all? WATSONN!!!! You are so cool, man!", WAIT_FOR_ME_F, null],
     				  [FRIEND2, "LOL", WAIT_FOR_ME_F, null],
-    				  [FRIEND3, "Hi, all!", WAIT_FOR_ME_F, null]]
+    				  [FRIEND3, "Hi, all!", WAIT_FOR_ME_F, null],
+    				  [WATSON, "Based on everyone's info, these seem like good options:", WAIT_FOR_ME_F, show_cities]]
 
     console.log(telescript);
 
@@ -66,7 +115,7 @@ $( document ).ready(function() {
 	    	}
     }
 
-    setTimeout(run_telescript, 1000);
+    setTimeout(run_telescript, 2000);
 
 
 
@@ -75,10 +124,22 @@ $( document ).ready(function() {
     	if(msg){
     		put_my_message("Maher", "img/maher_pro_pic.jpg", msg);
     		waiting_for_me = false;
+    		$("#input_msg").val("");
     	}
     	
-    	// test1();
-    	// put_friend_message("Logan", "img/maher_pro_pic.jpg", "What's popping?!");
+    });
+
+    document.addEventListener('keypress', function(e){
+    	if(e.keyCode == 13){
+    		var msg = $("#input_msg").val();
+	    	if(msg){
+	    		put_my_message("Maher", "img/maher_pro_pic.jpg", msg);
+	    		waiting_for_me = false;
+	    		$("#input_msg").val("");
+	    	}
+    	}
+    	
+    	
     });
 
     // var chat_window = document.getElementById("chat-window");
@@ -101,7 +162,7 @@ $( document ).ready(function() {
 				         </div>'
 		$("#chat_window").append(msg_element);
 		scrollToBottom();
-		setTimeout(run_telescript, 1000);
+		setTimeout(run_telescript, 2000);
     }
     
     function put_friend_message(u_name, u_pic_path, u_msg){
@@ -122,7 +183,7 @@ $( document ).ready(function() {
 				         </div>'
 		$("#chat_window").append(msg_element);
 		scrollToBottom();
-		setTimeout(run_telescript, 1000);
+		setTimeout(run_telescript, 2000);
     }
 
     function put_watson_message(u_name, u_pic_path, u_msg, made_live_scroll){
@@ -144,7 +205,12 @@ $( document ).ready(function() {
 				         </div>'
 		$("#chat_window").append(msg_element);
 		scrollToBottom();
-		setTimeout(run_telescript, 1000);
+		setTimeout(run_telescript, 2000);
+		$(".thumb").unbind();
+		$('.thumb').click(function() {
+	    	console.log("thumb clicked!");
+		    $(this).toggleClass('color_blue');
+		});
     }
 
     function make_live_scroll(ls_eles){
@@ -169,9 +235,9 @@ $( document ).ready(function() {
 		                  <li class="list-group-item">'+ ls_name +'</li>\
 		                  <li class="list-group-item list-group-item-secondary">\
 		                    <ul class="list-inline">\
-		                      <li class="list-inline-item active"><a href=""><i class="material-icons">thumb_up</i></a></li>\
+		                      <li class="list-inline-item active"><a><i class="material-icons thumb">thumb_up</i></a></li>\
 		                      <li class="list-inline-item active">|</li>\
-		                      <li class="list-inline-item active"><a href=""><i class="material-icons">thumb_down</i></a></li>\
+		                      <li class="list-inline-item active"><a><i class="material-icons thumb">thumb_down</i></a></li>\
 		                    </ul>\
 		                  </li>\
 		                </ul>\
@@ -198,12 +264,56 @@ $( document ).ready(function() {
     }
 
     function show_interests(){
-    	return "";
+    	var ls_eles = []
+    	for(var i=0; i<activities.length; i++){
+    		ls_eles.push(make_live_scroll_ele(activities[i][0], activities[i][1]));
+    	}
+    	var made_live_scroll = make_live_scroll(ls_eles);
+    	return made_live_scroll;
     }
 
     function invite_friends(){
+
+        setTimeout(function(){
+        	var invite_ele = '<div class="row justify-content-center">\
+	            <div class="card card_msg invite_card">\
+	              <div class="card-body card_body_msg text-center">\
+	                <p class="bottom_zero_margin_padding"><em>Watson added Logan to the chat<em></p>\
+	              </div>\
+	            </div>\
+	          </div>\
+	          <div class="row justify-content-center">\
+	            <div class="card card_msg invite_card">\
+	              <div class="card-body card_body_msg text-center">\
+	                <p class="bottom_zero_margin_padding"><em>Watson added Maher to the chat<em></p>\
+	              </div>\
+	            </div>\
+	          </div>\
+	          <div class="row justify-content-center">\
+	            <div class="card card_msg invite_card">\
+	              <div class="card-body card_body_msg text-center">\
+	                <p class="bottom_zero_margin_padding"><em>Watson added Matei to the chat<em></p>\
+	              </div>\
+	            </div>\
+	          </div>';
+	          $("#chat_window").append(invite_ele);
+	          scrollToBottom();
+        }, 250);
+
     	return "";
     }
+
+
+    function show_cities(){
+    	var ls_eles = []
+    	for(var i=0; i<activities.length; i++){
+    		ls_eles.push(make_live_scroll_ele(cities[i][0], cities[i][1]));
+    	}
+    	var made_live_scroll = make_live_scroll(ls_eles);
+    	return made_live_scroll;
+    }
+
+    
 
     
 
